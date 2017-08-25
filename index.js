9d44a86815a6b6cfd7f329708a64812b26bd13f8
jaimemendozadev <jaimemendozadev@gmail.com>
2017-08-24 18:21:57 -0700

    Setup router

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const router = require('./router');
 
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
+app.use('/api', router);
 
-app.get('/', (req, res)=>{
-  res.status(200);
-});
 
 app.listen(3000, ()=>{
   console.log("Listening on port 3000");
